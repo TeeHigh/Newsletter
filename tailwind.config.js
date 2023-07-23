e371af8bd24470fcc8d9eeb6943c89efdf978385
Ife <taiwoifeoluwa023@gmail.com>
2023-07-23 19:52:03 +0100

    Completed UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tomato: 'hsl(4, 100%, 67%)',
+        darkSlateGrey: 'hsl(234, 29%, 20%)',
+        charcoalGrey: 'hsl(235, 18%, 26%)',
+        grey: 'hsl(231, 7%, 60%)'
+      }
+    },
   },
   plugins: [],
 }
